139a3d2608ad10a1a5b9a89c158464425a453416
onizukaTP <tharunprabu2418@gmail.com>
2025-12-31 14:59:02 +0530

    UC3: extend the servlet to accept valid name

--- src/main/java/com/myFirstServlet/LoginServlet.java
+++ src/main/java/com/myFirstServlet/LoginServlet.java
@@ -9,6 +9,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.regex.Pattern;
 
 @WebServlet(
         description = "Login Servlet Testing",
@@ -19,19 +20,32 @@ import java.io.PrintWriter;
         }
 )
 public class LoginServlet extends HttpServlet {
+    private static final Pattern NAME_PATTERN = Pattern.compile("^[A-Z]{1}[a-zA-Z\\s]{2,}$");
+
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
         String user = request.getParameter("user");
         String pwd = request.getParameter("pwd");
 
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+
+        if (!NAME_PATTERN.matcher(user).matches()) {
+            out.println("<font color=red>Name is Incorrect</font>");
+            RequestDispatcher rd = getServletContext().getRequestDispatcher("/login.html");
+            rd.include(request, response);
+            return;
+        }
+
         String userID = getServletConfig().getInitParameter("user");
         String password = getServletConfig().getInitParameter("password");
+
         if (userID.equals(user) && password.equals(pwd)) {
             request.setAttribute("user", user);
             request.getRequestDispatcher("LoginSuccess.jsp").forward(request, response);
         } else {
             RequestDispatcher rd = getServletContext().getRequestDispatcher("/login.html");
-            PrintWriter out = response.getWriter();
             out.println("<font color=red>Either username or password is wrong.</font>");
             rd.include(request, response);
         }
